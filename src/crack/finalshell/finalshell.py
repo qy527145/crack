90dde6674dbf6525efcc43a463e1e6eb6f855400
wmymz <wmymz@icloud.com>
2024-03-11 00:33:40 +0800

    Add: 支持finalshell

---
+++ src/crack/finalshell/finalshell.py
@@ -0,0 +1,22 @@
+from Crypto.Hash import keccak
+from Crypto.Hash import MD5
+
+
+def keccak384(msg):
+    hash_obj = keccak.new(data=msg, digest_bits=384)
+    return hash_obj.hexdigest()
+
+
+def md5(msg):
+    hash_obj = MD5.new(msg)
+    return hash_obj.hexdigest()
+
+
+if __name__ == '__main__':
+    code = 'asd@1a4c215382a6d147'
+    print('版本号 < 3.9.6 (旧版)')
+    print('高级版:', md5(f'61305{code}8552'.encode())[8:24])
+    print('专业版:', md5(f'2356{code}13593'.encode())[8:24])
+    print('版本号 >= 3.9.6 (新版)')
+    print('高级版:', keccak384(f'{code}hSf(78cvVlS5E'.encode())[12:28])
+    print('专业版:', keccak384(f'{code}FF3Go(*Xvbb5s2'.encode())[12:28])
